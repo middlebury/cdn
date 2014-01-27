dc931c7901628e9d702539606fab99817a1d5d5b
Ian McBride <imcbride@middlebury.edu>
2014-01-27 11:13:37 -0500

    miis: Replaced references to $ with jQuery in JavaScript.

--- miis.edu/2010/js/miis.js
+++ miis.edu/2010/js/miis.js
@@ -12,17 +12,17 @@ var rollovers = {
 	fps:30 // frames per second
 };*/
 
-$(function() { // on DOM ready
+jQuery(function() { // on DOM ready
 	// Nav mouseovers
-	$('#miis_navigation').addClass('has_dropdowns').find('li').hover( // attach hover events to nav items
+	jQuery('#miis_navigation').addClass('has_dropdowns').find('li').hover( // attach hover events to nav items
 		function() { // onmouseenter
-			var self = $(this); // store the element
+			var self = jQuery(this); // store the element
 			clearTimeout(self.data('hideTimer')); // prevent hiding
 			self.data('showTimer',setTimeout(function() { // set (and store) the timeout
 					self.addClass('hover') // add the hover class
 						.siblings().removeClass('hover'); // and remove it from any siblings
 					self.find('input').focus(); // focus any input
-					if($.browser.msie&&$.browser.version>=7) { // if IE  /* DT-fix */
+					if(jQuery.browser.msie&&jQuery.browser.version>=7) { // if IE  /* DT-fix */
 						var feature = self.find('.nav_feature'),
 							copy = feature.clone();
 						feature.remove();
@@ -31,7 +31,7 @@ $(function() { // on DOM ready
 				},rollovers.beforeShow)
 			);
 		},function() { // onmouseleave
-			var self = $(this); // store the element
+			var self = jQuery(this); // store the element
 			clearTimeout(self.data('showTimer')); // prevent showing
 			self.data('hideTimer',setTimeout(function() { // set (and store) the timeout
 					self.removeClass('hover'); // add the hover class
@@ -41,29 +41,29 @@ $(function() { // on DOM ready
 		}
 	);
 	// Quickaccess
-	if($.fn.quickaccess) {
+	if(jQuery.fn.quickaccess) {
 		$('input.quickaccess').quickaccess({selector:'.qa_links a'});	
 	}
 	// Header photo caption popup
-	$('#miis_header_caption_more').click(function() {
-		$('#miis_header_caption_location,#miis_header_caption_more,#miis_header_caption_about').toggle();
-		$('#miis_header_caption').css('background-image', 'url()');
+	jQuery('#miis_header_caption_more').click(function() {
+		jQuery('#miis_header_caption_location,#miis_header_caption_more,#miis_header_caption_about').toggle();
+		jQuery('#miis_header_caption').css('background-image', 'url()');
 		return false;
 	});
-	$('#miis_header_caption_close').click(function() { /* DT-fix */
-		$('#miis_header_caption_more').click();
-		$('#miis_header_caption').css('background-image', 'url(http://www.miis.edu/sites/all/themes/miis/images/black_trans.png)');
+	jQuery('#miis_header_caption_close').click(function() { /* DT-fix */
+		jQuery('#miis_header_caption_more').click();
+		jQuery('#miis_header_caption').css('background-image', 'url(http://www.miis.edu/sites/all/themes/miis/images/black_trans.png)');
 		return false;
 	});
 	// FAQ and courses pages
-  $('.coursedesc').hide(); // Sep2-fix
-  $('.coursetitle').css('cursor','pointer').click(function() { // Sep2-fix
-    $(this).next().slideToggle();
+  jQuery('.coursedesc').hide(); // Sep2-fix
+  jQuery('.coursetitle').css('cursor','pointer').click(function() { // Sep2-fix
+    jQuery(this).next().slideToggle();
   });
 	if (!jQuery.browser.webkit) {
-	  $('details section').hide(); // Sep2-fix
-	  $('details summary').css('cursor','pointer').click(function() { // Sep2-fix
-	  	var dets = $(this).parent();
+	  jQuery('details section').hide(); // Sep2-fix
+	  jQuery('details summary').css('cursor','pointer').click(function() { // Sep2-fix
+	  	var dets = jQuery(this).parent();
 	    dets.find('section').slideToggle();
 	    if (dets.attr('open'))
 	    	dets.removeAttr('open');
@@ -73,27 +73,27 @@ $(function() { // on DOM ready
 	  });
 	 }
     // Sidebar Images
-    $('#miis_sidebar a img').parents('a').hover(
+    jQuery('#miis_sidebar a img').parents('a').hover(
       function() {
-        $(this).css('border', 'none');
+        jQuery(this).css('border', 'none');
       },
       function() {
-        $(this).css('border', 'none');
+        jQuery(this).css('border', 'none');
       }
     );
 
     if (window.location.pathname.indexOf('/admissions/apply', 0) == 0) {
-	    $('#block-monster-menus-1 .menu .menu .menu li:first-child a').each(function() {
-	    	var href = $(this).attr('href');
+	    jQuery('#block-monster-menus-1 .menu .menu .menu li:first-child a').each(function() {
+	    	var href = jQuery(this).attr('href');
 	    	if (href == '/admissions/apply/application') {
-	    		$(this).attr('href', 'https://ssb.middlebury.edu/PNTR/bzskalog.P_DispLoginNon');
-	    		$(this).attr('onClick', 'pageTracker._link(this.href); return false;');
+	    		jQuery(this).attr('href', 'https://ssb.middlebury.edu/PNTR/bzskalog.P_DispLoginNon');
+	    		jQuery(this).attr('onClick', 'pageTracker._link(this.href); return false;');
 	    	}
 	    });
 	}
 
-	$('.course_sections .sectiontitle').click(function() {
-		$(this).siblings().slideToggle();
+	jQuery('.course_sections .sectiontitle').click(function() {
+		jQuery(this).siblings().slideToggle();
 	});
 });
 
