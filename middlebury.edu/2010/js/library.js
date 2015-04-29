eb501a979bb71d5f38a53b2ae50b376d39a219c2
Ian McBride <imcbride@middlebury.edu>
2015-04-29 11:58:07 -0400

    midd: Add script to library.js to handle button clicks on the guides tab.

--- middlebury.edu/2010/js/library.js
+++ middlebury.edu/2010/js/library.js
@@ -1,3 +1,13 @@
 jQuery(function() {
   jQuery("#library-quick-search-tabs").tabs();
+
+  jQuery("#lib-guides input[type=submit]").click(function() {
+    var url = jQuery(this).siblings('select').eq(0).val();
+
+    if (url) {
+      window.location = url;
+    }
+
+    return false;
+  });
 });
