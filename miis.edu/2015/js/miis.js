a85c8e87383aa20d67dab9680b8c568fa0612f50
Ian McBride <imcbride@middlebury.edu>
2015-05-20 18:38:46 -0400

    miis: Final styles, images, and CSS for the first draft of the new MIIS homepage.

--- miis.edu/2015/js/miis.js
+++ miis.edu/2015/js/miis.js
@@ -1,5 +1,15 @@
 jQuery(function() {
-  jQuery('.languages li:first-child, .gateways li:first-child').click(function() {
+  jQuery('.languages header, .gateways header').click(function() {
     jQuery(this).siblings().toggle();
   });
+
+  if (jQuery('body').is('.front')) {
+    var maxHeight = Math.max.apply(null, jQuery('.columns .column').map(function () {
+      return jQuery(this).height();
+    }).get());
+  
+    jQuery('.front .column').each(function() {
+      jQuery(this).height(maxHeight);
+    });
+  }
 });
