3e2efeca1d7256b80b6519982424cdbbfbab48a3
Ian McBride <imcbride@middlebury.edu>
2015-09-08 10:42:36 -0400

    google_calendar: Allow the currently visible calendar to be set via the request hash. Web Helpdesk ticket 500852.

--- common/2010/js/google_calendar.js
+++ common/2010/js/google_calendar.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function() {
-  jQuery('#middlebury-google-calendar-0.middlebury_google_calendar').show();
+  var num = window.location.href.split('#')[1];
+
+  // If the hash value is not an integer, set it to 0.
+  if (isNaN(num) || num % 1 != 0) {
+    num = 0;
+  }
+
+  jQuery('#middlebury-google-calendar-' + num + '.middlebury_google_calendar').show();
+  jQuery('.midd_gcal_buttons option:eq(' + num + ')').prop('selected', true);
 
   jQuery('.midd_gcal_buttons').change(function() {
     var selected = jQuery(this).find('option:selected').index();
