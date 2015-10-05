45aa7ba6c9a1e16626d1e7e9de3ad1e998a49f90
Ian McBride <imcbride@middlebury.edu>
2015-10-05 10:14:21 -0400

    midd: Updated the Sustainability site to the latest CSS and JS from Zeb.

--- middlebury.edu/2015/js/sustainability.js
+++ middlebury.edu/2015/js/sustainability.js
@@ -1,2 +1,10 @@
+(function() {
+
+    jQuery('.menu-btn').on('click', function(e) {
+        e.preventDefault();
+        jQuery('#midd-menu').toggleClass('menu-open')
+    });
+
+})();
 
 //# sourceMappingURL=main.js.map
