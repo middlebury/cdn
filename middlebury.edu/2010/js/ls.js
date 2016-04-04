91a2530ca7ddeb48ba0e77ee0ba605a12c4d4315
Zeb Pykosz <zpykosz@middlebury.edu>
2016-04-04 16:08:16 -0400

    ls: add ls.js which inits the form validator plugin and polyfills placeholder if Modernizr detects no support

---
+++ middlebury.edu/2010/js/ls.js
@@ -0,0 +1,16 @@
+(function($) {
+
+  $(window).load(function() {
+    $('[data-validate-form').bsFormValidate();
+  });
+
+  if(!Modernizr.placeholder && Drupal.settings) {
+    (function(d, t) {
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+        g.src = Drupal.settings.middleburyUtility.middleburyCdnBase + '/common/2010/js/placeholders.jquery.min.js';
+        s.parentNode.insertBefore(g, s);
+    }(document, 'script'));
+  }
+
+})(jQuery);
